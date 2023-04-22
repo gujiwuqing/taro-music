03ccfd643b361dcf485e4709548359865d3c8eeb
孤寂无情 <15279279713@163.com>
2023-04-22 10:30:10 +0800

    首页banner

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,16 +1,42 @@
-import { View, Text } from '@tarojs/components'
-import { useLoad } from '@tarojs/taro'
+import {View, Text, Swiper, SwiperItem, Image} from '@tarojs/components';
 import './index.less'
+import Taro from '@tarojs/taro';
+import {useEffect, useState} from 'react';
 
-export default function Index() {
-
-  useLoad(() => {
-    console.log('Page loaded.')
+export default function Home() {
+  const [bannerList,setBannerList] = useState([])
+const getBanner = async ()=>{
+  const res = await  Taro.request({
+    url:'https://netease.gujiwuqing.top/banner?type=0',
   })
+  setBannerList(res.data?.banners||[])
+  console.log(res);
+}
 
+useEffect(()=>{
+  getBanner()
+},[])
   return (
     <View className='index'>
-      <Text>Hello world!</Text>
+      <Swiper
+        className='test-h'
+        indicatorColor='#999'
+        indicatorActiveColor='#333'
+        vertical={false}
+        circular
+        indicatorDots
+        autoplay
+      >
+        {
+          bannerList.map(item=>{
+            return (
+              <SwiperItem key={item.targetId}>
+                <Image src={item.imageUrl} style={{width:'100%'}}/>
+              </SwiperItem>
+            )
+          })
+        }
+      </Swiper>
     </View>
   )
 }
